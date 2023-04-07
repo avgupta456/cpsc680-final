84454aa8ad12a263ae5f7c6c2c1820a3595465e1
Abhijit Gupta <avgupta456@gmail.com>
2023-04-07 17:55:15 -0400

    Update method1.py

--- src/method1.py
+++ src/method1.py
@@ -1,12 +1,28 @@
 import matplotlib.pyplot as plt
-import numpy as np
 import torch
 
 from src.datasets import german
 
+
+def histogram(data_arr, label_arr, bins=100, density=True, alpha=0.5):
+    fig, ax = plt.subplots()
+    for data, label in zip(data_arr, label_arr):
+        ax.hist(
+            data,
+            bins=bins,
+            density=density,
+            alpha=alpha,
+            label=label,
+        )
+    ax.legend()
+    plt.show()
+
+
 if __name__ == "__main__":
-    node_model = torch.load("models/GermanDataset_Node_GCNConv(16,16,1)_Adam_500.pt")
-    edge_model = torch.load("models/GermanDataset_Edge_GCNConv(16,16)_Adam_500.pt")
+    plot = False
+
+    node_model = torch.load("models/GermanDataset_Node_GCNConv(16,1)_Adam_1000.pt")
+    edge_model = torch.load("models/GermanDataset_Edge_GCNConv(16)_Adam_1000.pt")
 
     data = german[0]
 
@@ -15,27 +31,43 @@ if __name__ == "__main__":
         node_model.embedding(data.x, data.edge_index), data.edge_index
     ).sigmoid()
 
-    fig, ax = plt.subplots()
-    ax.hist(edge_prob_1.detach().numpy(), bins=100)
-    ax.hist(edge_prob_2.detach().numpy(), bins=100)
-    plt.show()
-
-    edge_prob = edge_prob_1.detach().numpy()
+    if plot:
+        histogram(
+            [edge_prob_1.detach().numpy(), edge_prob_2.detach().numpy()],
+            ["Edge Model", "Node Model"],
+        )
 
     sens_attrs = data.sens_attrs.flatten().to(int)
     edge_homophily = sens_attrs[data.edge_index[0]] == sens_attrs[data.edge_index[1]]
 
-    # stacked histogram of edge prob 1 for homophily and non-homophily
-    fig, ax = plt.subplots()
-    ax.hist(
-        edge_prob[edge_homophily], bins=100, density=True, alpha=0.5, label="homophily"
+    if plot:
+        # stacked histogram of edge prob 1 for homophily and non-homophily
+        histogram(
+            [
+                edge_prob_1.detach().numpy()[edge_homophily],
+                edge_prob_1.detach().numpy()[~edge_homophily],
+            ],
+            ["Homophily", "Non-Homophily"],
+        )
+
+        # stacked histogram of edge prob 2 for homophily and non-homophily
+        histogram(
+            [
+                edge_prob_2.detach().numpy()[edge_homophily],
+                edge_prob_2.detach().numpy()[~edge_homophily],
+            ],
+            ["Homophily", "Non-Homophily"],
+        )
+
+    edge_prob = edge_prob_1
+
+    # Remove homophily edges with low edge probability
+    edge_index = data.edge_index[:, (edge_prob > 0.5) | ~edge_homophily]
+
+    print(
+        f"Removed {data.edge_index.shape[1] - edge_index.shape[1]} edges (out of {data.edge_index.shape[1]})"
     )
-    ax.hist(
-        edge_prob[~edge_homophily],
-        bins=100,
-        density=True,
-        alpha=0.5,
-        label="non-homophily",
-    )
-    ax.legend()
-    plt.show()
+
+    # Save to new dataset
+    data.edge_index = edge_index
+    torch.save(data, "data/german/processed/german_method_1.pt")
